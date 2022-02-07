a00ec3bf09772abe13f680c58430e16efde4d165
durga811 <96914435+durga811@users.noreply.github.com>
2022-02-08 00:32:23 +0530

    ffg

---
+++ team1.js
@@ -0,0 +1,138 @@
+var section1= document.getElementById("section1");
+var section2= document.getElementById("section2");
+var section3= document.getElementById("section3");
+var section4= document.getElementById("section4");
+var section5= document.getElementById("section5");
+var section6= document.getElementById("section6");
+var tab1= document.getElementById("tab1");
+var tab2= document.getElementById("tab2");
+var tab3= document.getElementById("tab3");
+var tab4= document.getElementById("tab4");
+var tab5= document.getElementById("tab5");
+var tab6= document.getElementById("tab6");
+function clicktab1(){
+ section1.style.display="block";
+    section2.style.display="none";
+    section3.style.display="none";
+    section4.style.display="none";
+    section5.style.display="none";
+    section6.style.display="none";
+    tab1.style.color="white";
+    tab2.style.color="black";
+    tab3.style.color="black";
+    tab4.style.color="black";
+    tab5.style.color="black";
+    tab6.style.color="black";
+    tab1.style.backgroundColor="rgb(18, 18, 175)";
+    tab2.style.backgroundColor="transparent";
+    tab3.style.backgroundColor="transparent";
+    tab4.style.backgroundColor="transparent";
+    tab5.style.backgroundColor="transparent";
+    tab6.style.backgroundColor="transparent";
+
+}
+function clicktab2(){
+    section2.style.display="block";
+    section1.style.display="none";
+    section3.style.display="none";
+    section4.style.display="none";
+    section5.style.display="none";
+    section6.style.display="none";
+    tab2.style.color="white";
+    tab1.style.color="black";
+    tab3.style.color="black";
+    tab4.style.color="black";
+    tab5.style.color="black";
+    tab6.style.color="black";
+    tab2.style.backgroundColor="rgb(18, 18, 175)";
+    tab1.style.backgroundColor="transparent";
+    tab3.style.backgroundColor="transparent";
+    tab4.style.backgroundColor="transparent";
+    tab5.style.backgroundColor="transparent";
+    tab6.style.backgroundColor="transparent";
+
+}
+function clicktab3(){
+ section3.style.display="block";
+    section2.style.display="none";
+    section1.style.display="none";
+    section4.style.display="none";
+    section5.style.display="none";
+    section6.style.display="none";
+    tab3.style.color="white";
+    tab2.style.color="black";
+    tab1.style.color="black";
+    tab4.style.color="black";
+    tab5.style.color="black";
+    tab6.style.color="black";
+    tab3.style.backgroundColor="rgb(18, 18, 175)";
+    tab2.style.backgroundColor="transparent";
+    tab1.style.backgroundColor="transparent";
+    tab4.style.backgroundColor="transparent";
+    tab5.style.backgroundColor="transparent";
+    tab6.style.backgroundColor="transparent";
+
+}
+function clicktab4(){
+ section4.style.display="block";
+    section2.style.display="none";
+    section3.style.display="none";
+    section1.style.display="none";
+    section5.style.display="none";
+    section6.style.display="none";
+    tab4.style.color="white";
+    tab2.style.color="black";
+    tab3.style.color="black";
+    tab1.style.color="black";
+    tab5.style.color="black";
+    tab6.style.color="black";
+    tab4.style.backgroundColor="rgb(18, 18, 175)";
+    tab2.style.backgroundColor="transparent";
+    tab3.style.backgroundColor="transparent";
+    tab1.style.backgroundColor="transparent";
+    tab5.style.backgroundColor="transparent";
+    tab6.style.backgroundColor="transparent";
+
+}
+function clicktab5(){
+ section5.style.display="block";
+    section2.style.display="none";
+    section3.style.display="none";
+    section4.style.display="none";
+    section1.style.display="none";
+    section6.style.display="none";
+    tab5.style.color="white";
+    tab2.style.color="black";
+    tab3.style.color="black";
+    tab4.style.color="black";
+    tab1.style.color="black";
+    tab6.style.color="black";
+    tab5.style.backgroundColor="rgb(18, 18, 175)";
+    tab2.style.backgroundColor="transparent";
+    tab3.style.backgroundColor="transparent";
+    tab4.style.backgroundColor="transparent";
+    tab1.style.backgroundColor="transparent";
+    tab6.style.backgroundColor="transparent";
+
+}
+function clicktab6(){
+ section6.style.display="block";
+    section2.style.display="none";
+    section3.style.display="none";
+    section4.style.display="none";
+    section5.style.display="none";
+    section1.style.display="none";
+    tab6.style.color="white";
+    tab2.style.color="black";
+    tab3.style.color="black";
+    tab4.style.color="black";
+    tab5.style.color="black";
+    tab1.style.color="black";
+    tab6.style.backgroundColor="rgb(18, 18, 175)";
+    tab2.style.backgroundColor="transparent";
+    tab3.style.backgroundColor="transparent";
+    tab4.style.backgroundColor="transparent";
+    tab5.style.backgroundColor="transparent";
+    tab1.style.backgroundColor="transparent";
+
+}
